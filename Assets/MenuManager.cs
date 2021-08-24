d0192d833b68e8cb354a1580ec3fce0a19ab3eb2
VinhBui01 <15buiv@johnmasonschool.org>
2021-08-24 20:19:34 +0100

    scene

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class MenuManager : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
+    public void PlayGame()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
 
     // Update is called once per frame
     void Update()
