91bca16b72105b5bf96b5f064fda6c780cdf09b0
VinhBui01 <15buiv@johnmasonschool.org>
2021-09-01 18:40:25 +0100

    menu

---
+++ Assets/DataController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : GameManager
+{
+    //public int Gamemode;
+
+    public void Offline()
+    { 
+    
+    }
+
+    public void Clear()
+    {
+
+    }
+    public void Online()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
