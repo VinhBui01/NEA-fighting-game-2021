6bab825df88dd707c1f30cc0cbb9bc941bd4f37b
VinhBui01 <15buiv@johnmasonschool.org>
2021-09-02 15:08:02 +0100

    stuff

---
+++ Assets/StartManager.cs
@@ -0,0 +1,24 @@
+using System.IO;
+
+public class StartManager
+{
+    //public int Gamemode;
+    public bool GameModeOnline;
+    public void offline()
+    {
+
+        GameModeOnline =  false;
+        Clear();
+    }
+
+    public void Clear() //reformats text file to empty
+    {
+        string[] lines = { "000", "P1 000 000", "P2 000 000" };
+        File.WriteAllLines("SaveFile.txt", lines);
+    }
+    public void online()
+    {
+        GameModeOnline = true;
+    }
+
+}
