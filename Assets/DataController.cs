f3952d4deb1378a2b751748d2e755b51ccaa604b
VinhBui01 <15buiv@johnmasonschool.org>
2021-09-02 14:21:21 +0100

    Update DataController.cs

--- Assets/DataController.cs
+++ Assets/DataController.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class DataController : GameManager
 {
     //public int Gamemode;
-
-    public void Offline()
-    { 
-    
+    public bool GameModeOnline;
+    public void offline()
+    {
+        GameModeOnline=  false;
     }
 
     public void Clear()
     {
+        string[] lines =
+        {
+            "P1 0 0 0", "P2 0 0 0"
+        };
 
+        File.WriteAllLines(@"C:unityprojects\NEA fighting game 2021\SaveFile.txt", lines);
     }
-    public void Online()
+    public void online()
     {
-
+        GameModeOnline = true;
     }
+    private void Start()
+    {
+        Clear();
+    }
+
     // Update is called once per frame
     void Update()
     {
