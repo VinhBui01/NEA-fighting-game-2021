ac0dc8476f8a5d06040238cc5bbf792723d88e40
VinhBui01 <15buiv@johnmasonschool.org>
2022-01-15 15:58:54 +0000

    database connection

---
+++ Assets/Registration.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+public class Registration : MonoBehaviour
+{
+    public TMP_InputField nameField;
+    public TMP_InputField passwordField;
+
+    public Button submitButton;
+    public void CallRegister() {
+        StartCoroutine(Register());
+
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+            form.AddField("name", nameField.text);
+            form.AddField("password", passwordField.text);
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form))
+        {
+            yield return www.SendWebRequest();
+            if (www.error == "0")
+            {
+                Debug.Log("User Created Successfully");
+                 
+
+            }
+            else
+            {
+                Debug.Log("User creation failed. Error #" + www.error);
+            }
+        }
+    }
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+}
