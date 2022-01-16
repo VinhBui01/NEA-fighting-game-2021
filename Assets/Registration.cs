623d1c852f7d381fdf9ff3cd9e532c9a1932b910
VinhBui01 <15buiv@johnmasonschool.org>
2022-01-16 16:51:22 +0000

    login

--- Assets/Registration.cs
+++ Assets/Registration.cs
@@ -10,9 +10,44 @@ public class Registration : MonoBehaviour
     public TMP_InputField passwordField;
 
     public Button submitButton;
-    public void CallRegister() {
-        StartCoroutine(Register());
+    public string[] P1Login = new string[2];
+    public string[] P2Login = new string[2];
+    public int LoginType;
+    private bool LoginSuccess;
+    public void CallRegister()
+    {
+        if (LoginType == 0) //logintype 0 is registration
+        {
+            StartCoroutine(Register());
 
+        }
+        else if (LoginType == 1)
+        {
+            LoginSuccess = false;
+            StartCoroutine(Login());
+            if (LoginSuccess == true)
+            {
+                P1Login[0] = nameField.text;
+                P1Login[1] = passwordField.text;
+            }
+
+        }
+        else if (LoginType == 2)
+        {
+            LoginSuccess = false;
+            StartCoroutine(Login());
+            if (LoginSuccess == true)
+            {
+                P2Login[0] = nameField.text;
+                P2Login[1] = passwordField.text;
+            }
+
+        }
+        else { Debug.Log(LoginType); }
+    }
+    public void SetLoginType(int num)
+    {
+        LoginType = num;
     }
 
     IEnumerator Register()
@@ -22,9 +57,9 @@ public class Registration : MonoBehaviour
             form.AddField("password", passwordField.text);
         UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form);
         yield return www.SendWebRequest();
-        if (www.downloadHandler.text == "0")
+        if (www.downloadHandler.text == "000")
         {
-            Debug.Log("User Created Successfully");
+            Debug.Log("Registration Successful");
         }
         else
         {
@@ -33,6 +68,28 @@ public class Registration : MonoBehaviour
         www.Dispose();
         
     }
+
+    IEnumerator Login()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/login.php", form);
+        yield return www.SendWebRequest();
+        if (www.downloadHandler.text == "100")
+        {
+            LoginSuccess = true;
+            Debug.Log("Successful Login");
+        }
+        else
+        {
+            Debug.Log("User creation failed. Error #" + www.downloadHandler.text);
+        }
+        www.Dispose();
+
+    }
+
+
     public void VerifyInputs()
     {
         submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
--- Assets/StartManager.cs
+++ Assets/StartManager.cs
@@ -1,21 +1,39 @@
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 public class StartManager : MonoBehaviour
 {
+    public string[] P1Login;
+    public string[] P2Login;
     //public int Gamemode;
     public void offline()
     {
 
-        Clear("false");
+        Clear();
     }
 
-    public void Clear(string GameModeOnline) //reformats text file to empty
+    public void Clear() //reformats text file to empty for offline start
     {
-        string[] lines = { GameModeOnline, "000", "P1 000 000", "P2 000 000" };
+        string[] lines = {"false", "000", "P1 000 000", "P2 000 000" };
         File.WriteAllLines("SaveFile.txt", lines);
     }
     public void online()
     {
-        Clear("true");
+        WWWForm form = new WWWForm();
+        form.AddField("P1name", P1Login[0]);
+        form.AddField("P1password", P1Login[1]);
+        form.AddField("P2name", P2Login[0]);
+        form.AddField("P2password", P2Login[1]);
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form);
+        yield return www.SendWebRequest();
+        if (www.downloadHandler.text == "000")
+        {
+            Debug.Log("Registration Successful");
+        }
+        else
+        {
+            Debug.Log("User creation failed. Error #" + www.downloadHandler.text);
+        }
+        www.Dispose();
     }
 }
